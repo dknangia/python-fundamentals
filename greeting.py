880701322b82be735bef93e3b3d3a07ff478cc1a
dknangia <dknangia@gmail.com>
2026-05-20 21:54:11 -0400

    Few adjustment to the program to accept the name from the user

---
+++ greeting.py
@@ -0,0 +1,6 @@
+name = 'Deepak\'s ball' # notice that we used another '\ to escape the single quote in the string.
+print("Hello, " + name) # notice we used + to concatenate the string. We can also use , to concatenate the string, but it will add a space between the strings.
+print("Hello", name) # using , to concatenate the string, it will add a space between the strings.
+
+userName = input("What is your name?\n") # notice the input() function, it is used to take the input from the user. It will return a string, so we can use it to concatenate with other strings.
+print("Hello, " + userName) 
