e2ecf4e97843d8d832b74875ca8cbc05b1dfa26e
dknangia <dknangia@gmail.com>
2026-05-20 22:21:37 -0400

    Simple age calculator

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,5 +1,9 @@
 age = int(input("How old are you?\n"))
-decades = age // 10
+decades =   age // 10
+
+
 years = age % 10
 print("You are are", str(decades) + " decades old, and " + str(years) + " year", years>1 and "s" or "" + " old.")
+
+
 
